bc082a4478ff4cc7c6f0cd6b4b4e87440a1bcb5a
dunqntd <dungnt3904@gmail.com>
2025-06-24 21:40:11 +0700

    dung3

--- MVC/Controllers/add2.php
+++ MVC/Controllers/add2.php
@@ -6,10 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>khac roi</h1>
-<<<<<<< HEAD
-    them 2 dong khac
-=======
+    <h1>khac roi</h1>   
+     them 2 dong khac
     them mot dong
 >>>>>>> 5cf63baa16925493a0c88ba7473d71aadc745eb1
 </body>
